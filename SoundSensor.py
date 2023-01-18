fb044728782ddbc88c6b1681f08f305bedee8f37
BrendanWoods03 <119702957+BrendanWoods03@users.noreply.github.com>
2023-01-18 11:16:17 -0600

    Update SoundSensor.py

--- SoundSensor.py
+++ SoundSensor.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(4, GPIO.IN, pull_up_down = GPIO.PUD)
+GPIO.setup(4, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 try:
     print("Test")
