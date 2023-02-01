3fe3effb6e90fea99be689f0bf34e324ad4d5057
hayleyowen <119703073+hayleyowen@users.noreply.github.com>
2023-02-01 11:33:00 -0600

    Update both.sensors.py

--- both.sensors.py
+++ both.sensors.py
@@ -3,49 +3,55 @@ import time
 from picamera import PiCamera
 from gpiozero import Buzzer
 
-#set up picamera
-camera = PiCamera()
+try:
+    #set up picamera
+    camera = PiCamera()
 
-#set sound as the GPIO pin the sound sensor is connected to
-sound = 6
+    #set sound as the GPIO pin the sound sensor is connected to
+    sound = 6
 
-#set motion as the GPIO pin the motion sensor is connected to
-motion = 4
+    #set motion as the GPIO pin the motion sensor is connected to
+    motion = 4
 
-#set up buzzer
-buzzer = Buzzer(24)
+    #set up buzzer
+    buzzer1 = Buzzer(23)
+    buzzer2 = Buzzer(26)
 
-#set the GPIO to broadcom mode
-GPIO.setmode(GPIO.BCM)
+    #set the GPIO to broadcom mode
+    GPIO.setmode(GPIO.BCM)
 
-#set the pins as inputs
-GPIO.setup(sound, GPIO.IN)
-GPIO.setup(motion, GPIO.IN)
+    #set the pins as inputs
+    GPIO.setup(sound, GPIO.IN)
+    GPIO.setup(motion, GPIO.IN)
 
-#check if sound sensor is detecting sound
-def detection(sound):
-    if GPIO.input(sound):
-        print("Sound Detected")
-        buzzer.on()
-    else:
-        print("Sound Detected")
+    #check if sound sensor is detecting sound
+    def detection_sound(sound):
+        if GPIO.input(sound):
+            print("Sound Detected")
+            buzzer1.beep()
+        else:
+            print("Sound Detected")
 
-#check if motion sensor is detecting motion
-def detection(motion):
-    if GPIO.input(motion):
-        print("Motion Detected")
-        #take picture when motion is detected
-        camera.capture('/home/pi/Desktop/PersonSpotted.jpg')
-    else:
-        pass
+    #check if motion sensor is detecting motion
+    def detection_motion(motion):
+        if GPIO.input(motion):
+            print("Motion Detected")
+            #take picture when motion is detected
+            camera.capture('/home/pi/Desktop/PersonSpotted.jpg')
+            buzzer2.on()
+        else:
+            pass
+
+    #
+    GPIO.add_event_detect(sound, GPIO.BOTH, bouncetime = 300)
+    GPIO.add_event_callback(sound, detection_sound)
+
+    #
+    GPIO.add_event_detect(motion, GPIO.BOTH, bouncetime = 300)
+    GPIO.add_event_callback(motion, detection_motion)
+
+    while True:
+        time.sleep(1)
         
-#
-GPIO.add_event_detect(sound, GPIO.BOTH, bouncetime = 300)
-GPIO.add_event_callback(sound, detection)
-
-#
-GPIO.add_event_detect(motion, GPIO.BOTH, bouncetime = 300)
-GPIO.add_event_callback(motion, detection)
-
-while True:
-    time.sleep(1)
+ except KeyboardInterrupt:
+    GPIO.cleanup()
